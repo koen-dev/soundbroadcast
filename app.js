2ea396f5a45fa874fbf3c877eca5ce1b08dcd726
Koen Hendriks <k.hendriks@graciousstudios.nl>
2017-09-24 19:47:14 +0200

    Changes

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
 const http =  require('http');
 const fs   =  require('fs');
 const path =  require('path');
+const config = require('./config/server');
+const server = http.createServer(handler);
+const io = require('socket.io')(server);
+const open = require('opn');
+const apiRegex = /^\/api/i;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end('okay');
+function handler(req, res){
+    if (req.url.match(apiRegex)) {
+        res.writeHead(500);
+        res.end('No API implemented.');
+    }else{
+        if (req.method === "GET") {
+            getFile(req, res);
+        }
+    }
+    console.log(`path: ${req.url}, method: ${req.method}`);
+};
+
+const getFile = (req, res) => {
+    let file = (req.url === "/") ? "/index.html" : req.url;
+    let resolvedPath = path.join(__dirname, file);
+    if (fs.existsSync(resolvedPath)) {
+        res.writeHead(200);
+        res.end(fs.readFileSync(resolvedPath, 'utf-8'));
+    }else{
+        res.writeHead(404);
+        res.end('File not found.');
+    }
+}
+
+io.on('connection', (socket) => {
+    socket.on('message', (data) => {
+        console.log(data);
+        socket.emit('news', { hello: 'world' });
+    });
 });
 
-server.listen(8080);
+server.listen(config.port);
+console.log(`Server listening on port ${config.port}`);
+//open(`http://localhost:${8080}`);
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './components/Main';
+import io from 'socket.io-client';
+
+let socket = io('http://localhost:8080');
 
 ReactDOM.render(
   <Main/>,
   document.getElementById('app')
 );
+
+socket.on('connect', () => {
+    console.log('Socket connected!');
+});
+
+socket.on('news', (data) => {
+    console.log(data);
+});
+
+window.test = (msg) => {
+    socket.emit('message', msg);
+};
